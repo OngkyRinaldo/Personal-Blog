5fd3fa2c758b0be23af808c9f1e496e360109cfe
edoorinaldoo@gmail.com <edoorinaldoo@gmail.com>
2022-08-21 21:22:09 +0700

    upload

---
+++ app/Models/Tag.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Tag extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'slug'
+    ];
+
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class);
+    }
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+}
