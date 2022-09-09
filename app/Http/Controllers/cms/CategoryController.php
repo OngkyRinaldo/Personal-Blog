40396798af2f3ab0b4cfbc3b76bbe20b097c7407
edoorinaldoo@gmail.com <edoorinaldoo@gmail.com>
2022-09-09 22:32:32 +0700

    update

--- app/Http/Controllers/cms/CategoryController.php
+++ app/Http/Controllers/cms/CategoryController.php
@@ -109,6 +109,6 @@ class CategoryController extends Controller
         $category->delete();
 
         return redirect()->route('category.index')
-            ->with('success', 'Category has been delated');
+            ->with('success', 'Category has been deleted');
     }
 }
