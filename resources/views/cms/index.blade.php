a5cab7a38e70a207ce1e87bdf221d0f3a53c351a
edoorinaldoo@gmail.com <edoorinaldoo@gmail.com>
2022-09-13 15:49:00 +0700

    update

--- resources/views/cms/index.blade.php
+++ resources/views/cms/index.blade.php
@@ -7,47 +7,70 @@ Dashboard
 @section('main')
 
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+
     <div
         class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+
         <div class="container">
+
             <div class="row">
+
                 <div class="col">
+
                     <div class="text-center my-3">
 
                         <h1>Welcome {{ $user }}</h1>
+
                     </div>
                     <h2>All posts</h2>
+
                     <div class="table-responsive">
+
                         <table class="table table-striped table-sm">
+
                             <thead>
+
                                 <tr>
+
                                     <th scope="col">No.</th>
                                     <th scope="col">Title</th>
                                     <th scope="col">Category</th>
                                     <th scope="col">Descriptions</th>
                                     <th scope="col">Author</th>
+
                                 </tr>
+
                             </thead>
+
                             <tbody>
+
                                 @foreach ($posts as $post)
+
                                 <tr>
+
                                     <td>{{ $loop->iteration }}</td>
                                     <td>{{ $post->title }}</td>
                                     <td>{{ $post->category->title }}</td>
                                     <td>{{Str::limit($post->descriptions, 46) }}</td>
                                     <td>{{ $post->post_author->username }}</td>
+
                                 </tr>
+
                                 @endforeach
 
                             </tbody>
+
                         </table>
+
                     </div>
+
                 </div>
+
             </div>
+
         </div>
+
     </div>
-
-
 
 </main>
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,85 +5,151 @@ Personal Blog
 @endsection
 
 @section('content')
+
 <section id="headline">
+
     <div class="container mb-3">
+
         <div class="row justify-content-end mt-4">
-            <div class="col-md-6">
+
+            <div class="col-md-6 col-6">
+
                 <form action="{{ route('guest.index') }}">
+
                     <div class="input-group mb-3">
+
                         <input type="text" class="form-control" placeholder="search..." name="search"
                             value="{{ request('search') }}">
+
                         <button class="btn btn-dark" type="submit">Search</button>
+
                     </div>
+
                 </form>
+
             </div>
+
         </div>
+
         @if ($posts->count())
+
         <div class="row mt-3 row-headline">
+
             <div class="col-md-5">
+
                 <figure class="figure p-3">
+
                     @if ($posts[0]->image)
+
                     <img src=" {{ asset('images/post/' . $posts[0]->image) }}" class="figure-img img-fluid" alt="{{
                     $posts[0]->title }}">
+
                     @else
+
                     <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->title }}"
                         class="figure-img img-fluid" alt="{{ $posts[0]->title }}">
+
                     @endif
+
                     <figcaption class="figure-caption text-center">{{ $posts[0]->title }}</figcaption>
+
                 </figure>
+
             </div>
+
             <div class="col-md-7">
-                <h1 class="fw-bold h1-headline"><a href="{{ route('guest.post', $posts[0]) }}"
-                        class="text-decoration-none text-dark">{{Str::limit($posts[0]->title, 46) }} </a></h1>
-                <a href="{{ route('guest.category', $posts[0]->category) }}"><button class="btn btn-dark mb-3"
-                        type="button">{{ $posts[0]->category->title
-                        }}</button></a>
-                <h3 class="headline-caption fs-4 text-muted">{{$posts[0]->descriptions}}</< /h3>
-                    <div class="text-end mt-3">
-                        <a href="{{ route('guest.post', $posts[0]) }}"><button type="button" class="btn btn-dark">Read
-                                more!</button></a>
-                    </div>
+
+                <h1 class="fw-bold h1-headline">
+                    <a href="{{ route('guest.post', $posts[0]) }}"
+                        class="text-decoration-none text-dark">{{Str::limit($posts[0]->title, 46) }}
+                    </a>
+                </h1>
+
+                <a href="{{ route('guest.category', $posts[0]->category) }}">
+
+                    <button class="btn btn-dark mb-3" type="button">{{ $posts[0]->category->title}}</button>
+
+                </a>
+
+                <h3 class="headline-caption fs-4 text-muted">{{$posts[0]->descriptions}}</h3>
+
+                <div class="text-end mt-3">
+
+                    <a href="{{ route('guest.post', $posts[0]) }}">
+
+                        <button type="button" class="btn btn-dark">Read more!</button>
+                    </a>
+
+                </div>
+
             </div>
         </div>
+
     </div>
 
 </section>
 
 <section id="card">
+
     <div class="container mb-3">
 
         <div class="row row-card-index">
+
             @foreach ($posts->skip(1) as $post)
+
             <div class="col-lg-4 col-md-4 col-6 mt-4">
+
                 <div class=" card card-index">
-                    <div class=" position-absolute p-3 py-2 card-category-title text-center"><a
-                            href="{{ route('guest.category', $posts[0]->category) }}">
-                            {{ $post->category->title }}</a>
+
+                    <div class=" position-absolute p-3 py-2 card-category-title text-center">
+
+                        <a href="{{ route('guest.category', $posts[0]->category) }}">
+                            {{ $post->category->title }}
+                        </a>
+
                     </div>
+
                     <div class="image-card text-center p-1">
 
                         @if ($post->image)
+
                         <img src="{{ asset('images/post/' . $post->image) }}" class="figure-img img-fluid"
                             alt="{{ $post->title }}">
                         @else
+
                         <img src="https://source.unsplash.com/1200x400?{{ $post->category->title }}"
                             class="figure-img img-fluid" alt="{{ $post->title }}">
+
                         @endif
 
                     </div>
 
                     <div class="card-body">
-                        <h5 class="card-title text-center"><a href="{{ route('guest.post', $post) }}"
+
+                        <h5 class="card-title text-center">
+
+                            <a href="{{ route('guest.post', $post) }}"
                                 class="text-decoration-none text-dark">{{Str::limit($post->title, 46) }}
-                            </a></h5>
+                            </a>
+
+                        </h5>
+
                         <hr>
+
                         <a href="{{ route('guest.post', $post) }}" class="btn btn-dark d-grid">Read-more</a>
+
                     </div>
+
                 </div>
+
             </div>
+
             @endforeach
+
         </div>
+
     </div>
+
 </section>
 
 
